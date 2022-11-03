2a556d2bbcf077829ca363f4c97ce2e9b67dfbb4
NiTiSon <NiTiSon@users.noreply.github.com>
2022-11-03 22:09:47 +0300

    Add project files.

---
+++ BotExecute/Program.cs
@@ -0,0 +1,8 @@
+﻿using NiTiS.Bots.NiTiSMaid;
+
+Bot bot = new(new File("token.txt").ReadAllText(), new(true, new ulong[]
+{
+	508012163307143168,
+}));
+
+bot.Start();
---
+++ NiTiSMaid/Bot.cs
@@ -0,0 +1,12 @@
+﻿namespace NiTiS.Bots.NiTiSMaid;
+
+public class Bot
+{
+	private readonly Configuration conf;
+	public Bot(string token, Configuration config)
+	{
+
+	}
+
+	public record class Configuration(bool Debug, ulong[] OwnerIDs);
+}
